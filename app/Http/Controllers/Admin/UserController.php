acad48c9b740f20c9e5e59e4ac47f04c9b0e1bec
persevrance <yogadhi@pnb.ac.id>
2026-04-08 20:37:50 +0800

    fixed

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
 
     public function index()
     {
-        return view('pages.admin.users.index', [
+        return view('pages.admin.users.create', [
             'users' => User::latest()->get()
         ]);
     }
